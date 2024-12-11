e4040d34fee33f2b279555d380db96257e2d255a
fizaank.phy21.iitbhu <fizaan.khan.phy21@iitbhu.ac.in>
2024-12-11 19:22:50 +0530

    update

--- starter.sh
+++ starter.sh
@@ -3,5 +3,10 @@
 #SBATCH --output=optim.out
 #SBATCH --time=4-00:00:00
 #SBATCH --ntasks=7
+#SBATCH -A physics_engg
+
+source ~/.bashrc
+
+cd cavityCoupling
 
 time mpiexec.hydra -n $SLURM_NTASKS julia fit_plot.jl
