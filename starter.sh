b46cfa9e660ac2855f1ae1a85aa097c41cbde098
FazinHan <freak.fizaan@gmail.com>
2024-12-11 18:43:23 +0530

    update peak widths data files for consistency; modify starter.sh to streamline job execution

--- starter.sh
+++ starter.sh
@@ -4,15 +4,4 @@
 #SBATCH --time=4-00:00:00
 #SBATCH --ntasks=7
 
-echo "========= Job started  at `date` on `hostname -s` =========="
-
-#export I_MPI_HYDRA_TOPOLIB=ipl
-#export OMP_NUM_THREADS=1
-
-echo "Job id       : $SLURM_JOB_ID"
-
-
 time mpiexec.hydra -n $SLURM_NTASKS julia fit_plot.jl
-
-
-echo "========= Job finished at `date` =========="
