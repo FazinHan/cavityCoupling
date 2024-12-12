63e914b372a0b4950cf196b3c92bf0afbfcbb71a
FazinHan <freak.fizaan@gmail.com>
2024-12-12 13:13:06 +0530

    fix starter.sh: update SLURM directives for task allocation and modify Julia executable path

--- starter.sh
+++ starter.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#SBATCH -N 1
 #SBATCH --job-name=g1.g2
 #SBATCH --output=optim.out
 #SBATCH --time=4-00:00:00
-#SBATCH --ntasks=7
+#SBATCH --ntasks-per-node=7
 #SBATCH -A physics_engg
+#SBATCH --mail-user=fizaan.khan.phy21@iitbhu.ac.in
 
 source ~/.bashrc
 
-time mpiexec -n $SLURM_NTASKS julia fit_plot.jl
+time mpiexec -n $SLURM_NTASKS /scratch/fizaank.phy21.iitbhu/julia-1.11.2/bin/julia fit_plot.jl
