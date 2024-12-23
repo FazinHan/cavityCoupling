a0a219ab59649fab2ff2e80f3c5a3201decb3b96
FazinHan <freak.fizaan@gmail.com>
2024-12-23 22:34:09 +0530

    fix: adjust ω2n value in fit_plot.jl; update peak location assignment in peak_aware_fitter3 function

--- s21_optimiser.m
+++ s21_optimiser.m
@@ -21,7 +21,6 @@ function fit_params_matrix = peak_aware_fitter3(filename, min_prominence, fine_s
     for col=1:num_data
         smoothed_data = smooth(s21_full(:,col),fine_smoothing, 'lowess');
         [pks, locs, widths, prominence] = findpeaks(-1*smoothed_data, frequencies,'MinPeakProminence',min_prominence,'MinPeakDistance',.1,'SortStr','descend','NPeaks',2);
-        % fit_params_matrix(1:2,col) = smooth(locs,coarse_smoothing, 'lowess');
         fit_params_matrix(1:2,col) = locs;
         fit_params_matrix(3:4,col) = widths;
     end
