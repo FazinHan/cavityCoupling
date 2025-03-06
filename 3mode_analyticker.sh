b6096ebfb1689ddd0a69fc2d19515db755701d6c
FazinHan <freak.fizaan@gmail.com>
2025-03-06 15:05:50 +0530

    feat: add 3mode_analyticker.sh script for job management and output handling

---
+++ 3mode_analyticker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --job-name=3mode.analyticker
+#SBATCH --output=output.out
+#SBATCH --time=4-00:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH -A physics_engg
+#SBATCH --mail-user=fizaan.khan.phy21@iitbhu.ac.in
+
+WORKDIR=/job_outputs/$SLURM_JOB_ID
+mkdir -p "$WORKDIR" && cd "$WORKDIR" || exit -1
+
+echo "========= Job started  at `date` on `hostname -s` =========="
+
+#export I_MPI_HYDRA_TOPOLIB=ipl
+#export OMP_NUM_THREADS=1
+
+echo "Job id       : $SLURM_JOB_ID"
+
+export I_MPI_FALLBACK=disable
+export I_MPI_FABRICS=shm:tmi
+export I_MPI_DEBUG=9 
+export OMP_NUM_THREADS=40
+
+source ~/.bashrc
+
+cd cavityCoupling
+
+time mpiexec.hydra -genv I_MPI_DEBUG 9 -n $SLURM_NTASKS -genv OMP_NUM_THREADS 1 /scratch/fizaank.phy21.iitbhu/anaconda3/bin/python triple_mode_sweeps.py
+
+if [ -f "S.p" ]; then
+    echo "File S.p created."
+else
+    echo "File S.p not created."
+fi
+
+mv output.out $WORKDIR/output.out
+mv S.p $WORKDIR/S.p
+
+echo "========= Job finished at `date` =========="
