83a224962cdcfc30806ae4e7265ef624c81fcdc7
FazinHan <freak.fizaan@gmail.com>
2025-03-06 10:55:57 +0530

    feat: update 3mode_analyticker.sh to call triple_mode_sweeps.py; add triple_mode_sweeps.py script and tutorials.ipynb for analysis

--- 3mode_analyticker.sh
+++ 3mode_analyticker.sh
@@ -23,7 +23,7 @@ source ~/.bashrc
 
 cd cavityCoupling
 
-time mpiexec.hydra -genv I_MPI_DEBUG 9 -n $SLURM_NTASKS -genv OMP_NUM_THREADS 1 /scratch/fizaank.phy21.iitbhu/anaconda3/bin/python 3mode_analytical_solver.py
+time mpiexec.hydra -genv I_MPI_DEBUG 9 -n $SLURM_NTASKS -genv OMP_NUM_THREADS 1 /scratch/fizaank.phy21.iitbhu/anaconda3/bin/python triple_mode_sweeps.py
 
 if [ -f "S.p" ]; then
     echo "File S.p created."
