2471902609188d3611a219518256f81443c7ef13
FazinHan <freak.fizaan@gmail.com>
2025-03-11 12:23:12 +0530

    feat: add data analysis script for S-parameter visualization using matplotlib

---
+++ analyse_ansys_data.py
@@ -0,0 +1,19 @@
+import os, csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file = os.path.join('data', 'S Parameter Plot 2.csv')
+
+data = pd.read_csv(file, header=0)
+
+pivoted =  data.pivot(columns='bias []', index='Freq [GHz]', values='dB(S(2,1)) []')
+
+# print(pivoted)
+
+s21_array = pivoted.to_numpy()
+
+plt.pcolormesh(pivoted.columns*1e-3, pivoted.index, s21_array, cmap='jet')
+plt.xlabel('Bias [kA/m]')
+plt.ylabel('Frequency [GHz]')
+plt.colorbar()
+plt.show()
