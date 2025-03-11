324cf42def441cb86ce0d24c49d090e6a7e58705
FazinHan <freak.fizaan@gmail.com>
2025-03-11 12:26:56 +0530

    feat: update visualization in analyse_ansys_data.py to use 'inferno' colormap and save output as PNG

--- analyse_ansys_data.py
+++ analyse_ansys_data.py
@@ -12,8 +12,8 @@ pivoted =  data.pivot(columns='bias []', index='Freq [GHz]', values='dB(S(2,1)) 
 
 s21_array = pivoted.to_numpy()
 
-plt.pcolormesh(pivoted.columns*1e-3, pivoted.index, s21_array, cmap='jet')
+plt.pcolormesh(pivoted.columns*1e-3, pivoted.index, s21_array, cmap='inferno')
 plt.xlabel('Bias [kA/m]')
 plt.ylabel('Frequency [GHz]')
 plt.colorbar()
-plt.show()
+plt.savefig("results\\ansys_sweep.png")
